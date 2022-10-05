3bcc5db9c77b1601bd548ef8e964761c046947f3
montaserRahmani <montaser.rah@gmail.com>
2022-10-05 16:18:54 +0300

    added basic template for footer and header and apply it globally + product module code structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ProductModule } from './product/product.module';
+import { ShellModule } from './shell/shell.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HomeModule,
+    ProductModule,
+    ShellModule,
     AppRoutingModule
   ],
   providers: [],
